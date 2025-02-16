e0efebd4135e7885641a01a89582e147be9151f2
Alex Yun <iamalexyundev@gmail.com>
2025-02-16 23:18:32 +0900

    finished converter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Temperature converter!");
+    println!("Please input the temperature you want to convert from:");
+
+    let temperature: f64 = loop {
+        match get_user_input().trim().parse() {
+            Ok(temp) => break temp,
+            Err(_) => println!("Invalid input! Please enter a number!"),
+        }
+    };
+
+    println!("Please input the unit you want to convert from:");
+
+    let unit = loop {
+        match get_user_input().trim().to_lowercase().as_str() {
+            "f" => {
+                println!("Converting {temperature} from F to C");
+                break "F";
+            }
+            "c" => {
+                println!("Converting {temperature} from C to F");
+                break "C";
+            }
+            _ => println!("Invalid input! Please enter 'C' or 'F'."),
+        }
+    };
+    println!("You entered: {:.2}°{}", temperature, unit);
+    let converted = convert(temperature, unit);
+    println!(
+        "{temperature:.2}°{unit} is {:.2}°{}",
+        converted.0, converted.1
+    )
+}
+
+fn get_user_input() -> String {
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read input");
+    user_input
+}
+
+fn convert(temperature: f64, symbol: &str) -> (f64, &str) {
+    match symbol {
+        "F" => return ((temperature - 32.0) * 5.0 / 9.0, "C"),
+        "C" => return (temperature * (9.0 / 5.0) + 32.0, "F"),
+        _ => {
+            println!("Something went wrong!");
+            return (0.0, "Error");
+        }
+    };
 }
